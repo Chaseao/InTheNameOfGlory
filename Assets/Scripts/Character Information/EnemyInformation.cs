55b23917b3fee5dcb9dd437687f0e6da4ca5182c
Keit Vu <keitvu@gmail.com>
2022-03-26 16:22:09 -0400

    gitignore and enemy action
    Updated gitignore and added new information to enemy script.

--- Assets/Scripts/Character Information/EnemyInformation.cs
+++ Assets/Scripts/Character Information/EnemyInformation.cs
@@ -3,10 +3,12 @@
 [CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
 public class EnemyInformation : CharacterInformation
 {
-    [SerializeField] ActionInformation enemyAction;
+    [SerializeField] ActionInformation enemyAction1;
+    [SerializeField] ActionInformation enemyAction2;
     [SerializeField] int initialGold;
 
-    public ActionInformation EnemyAction => enemyAction;
+    public ActionInformation EnemyAction1 => enemyAction1;
+    public ActionInformation EnemyAction2 => enemyAction2;
     public int InitialGold => initialGold;
 }
 
